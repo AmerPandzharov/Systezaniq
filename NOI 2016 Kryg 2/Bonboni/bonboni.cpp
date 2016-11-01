11036585cb43d83f8b2ddf37da1f0b93eaad68be
Merhat <merhat1998@gmail.com>
2016-11-01 20:29:00 +0200

    Mojebi raboti

--- NOI 2016 Kryg 2/Bonboni/bonboni.cpp
+++ NOI 2016 Kryg 2/Bonboni/bonboni.cpp
@@ -1,8 +1,71 @@
 #include<iostream>
+#include<cmath>
+#include<algorithm>
 
 using namespace std;
 
+long minNumber(long a, long b)
+{
+    if(a > b)
+    {
+        return b;
+    }
+
+    return a;
+}
+
 int main()
 {
+    long a,b,c;
+    cin>>a>>b>>c;
+    long res = 0;
+
+    res+=3;
+    a -= 1;
+    b -= 1;
+    c -= 1;
+
+    bool isFirst = true;
+
+    while(true)
+    {
+        if(a == 0 || b == 0 || c == 0)
+        {
+            break;
+        }
+
+        if(isFirst)
+        {
+            a--;
+        }
+        else
+        {
+            c--;
+        }
+
+        b--;
+
+        isFirst = !isFirst;
+
+        res += 2;
+    }
+
+    if(a == 0 || c == 0)
+    {
+        if(b - 1 == 0)
+        {
+            res += 2;
+        }
+        else if (b - 1 > 0)
+        {
+            res += 3;
+        }
+    }
+    else
+    {
+        res += 1;
+    }
+
+    cout<<res<<endl;
     return 0;
 }
