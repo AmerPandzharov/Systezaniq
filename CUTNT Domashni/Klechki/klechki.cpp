b371ec1d335232f2d56f046dee59628a74abbad1
Merhat <merhat1998@gmail.com>
2016-11-01 17:40:24 +0200

    Domashnoto po cutnt kle4ki e ka4eno.

---
+++ CUTNT Domashni/Klechki/klechki.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int sk1,fk1,sk2,fk2,sk3,fk3;
+	int dul1,dul2,dul3;
+	cin>>sk1>>fk1>>sk2>>fk2>>sk3>>fk3;
+	dul1 = fk1 - sk1;
+	dul2 = fk2 - sk2;
+	dul3 = fk3 - sk3;
+	if(dul1 > dul2) swap(dul1,dul2);
+	if(dul1 > dul3) swap(dul1,dul3);
+	if(dul2 > dul3) swap(dul2,dul3);
+	if(fk1 >= sk2&&fk2 >= sk3)
+    {
+        cout<<1<<endl;
+        cout<<dul1<<" "<<dul3<<endl;
+    }
+    else if(fk1 >= sk2&&fk2 < sk3)
+    {
+        cout<<2<<endl;
+        cout<<dul1<<" "<<dul3<<endl;
+    }
+    else if(fk2 >= sk3)
+    {
+        cout<<2<<endl;
+        cout<<dul1<<" "<<dul3<<endl;
+    }
+    else
+    {
+        cout<<3<<endl;
+        cout<<dul1<<" "<<dul3<<endl;
+    }
+    return 0;
+}
